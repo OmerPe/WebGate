10feef23badaf078ea15ab1decd2332235ca532f
DePawk <sinfff@gmail.com>
2021-06-02 13:50:51 +0100

    added location finder/  targeted port and ip block and made a ai prepartion module

--- src/main/java/DNSLookUp.java
+++ src/main/java/DNSLookUp.java
@@ -9,10 +9,10 @@ public class DNSLookUp {
     public static ArrayList<String> lookup(Site s) {
         ArrayList<String> output = new ArrayList<>();
         InetAddress[] myHost;
-
+        String test=SiteCheck(s);
         // Print the IP addresses
         try {
-            myHost = InetAddress.getAllByName(s.getDomain());
+            myHost = InetAddress.getAllByName(test);
             for (InetAddress ad:
                     myHost) {
                 output.add(ad.getHostAddress());
@@ -24,5 +24,23 @@ public class DNSLookUp {
 
         s.addMultipleIp(output);
         return output;
+    }
+    public static String SiteCheck(Site s)
+    {
+        int size =s.getDomain().length();
+        String tmp=s.getDomain().substring(0,6);
+        if(tmp.equals("http:"))
+        {
+            String result=s.getDomain().substring(11,size);
+            System.out.println(result);
+            return result;
+        }
+        else if(tmp.equals("https"))
+        {
+            String result=s.getDomain().substring(12,size);
+            System.out.println(result);
+            return result;
+        }
+        return s.getDomain();
     }
 }
